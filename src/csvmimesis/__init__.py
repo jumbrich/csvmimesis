c5cb30fc984f6313e4212a8d54578fc5ed2ea9dc
jumbrich <jueumb@gmail.com>
2018-05-15 15:32:54 +0200

    add version info

--- src/csvmimesis/__init__.py
+++ src/csvmimesis/__init__.py
@@ -6,7 +6,7 @@ try:
     dist_name = __name__
     __version__ = get_distribution(dist_name).version
 except DistributionNotFound:
-    __version__ = 'unknown'
+    __version__ = '0.1.0'
 
 
 import logging
